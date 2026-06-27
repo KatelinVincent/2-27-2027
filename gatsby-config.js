109ec1945dcd34ac1e59249f8aa12bfa5e2b6172
Katelin Vincent <katelinannvincent@gmail.com>
2026-06-27 14:07:24 -0400

    remove prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  pathPrefix: "/2-27-2027",
   siteMetadata: {
     title: "Jacob & Kate Nemeth",
     description: "Join us as we become the Nemeths — February 27, 2027, Texas Hill Country.",
